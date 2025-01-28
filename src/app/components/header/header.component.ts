1aec2997a6e9b791183fe89f7646693292144c19
Rafael Ferreira <rafabf18@gmail.com>
2025-01-28 15:54:15 -0300

    feat: header update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PizzaModel } from '../../models/PizzaModel';
+import { SharedService } from '../../services/shared.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +13,33 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isMenuOpen = false;
+  cart: PizzaModel[] = [];
+  pizzaEventSubscription: Subscription;
+  isCartDropdownOpen = false;
 
+  constructor(private sharedService:SharedService){
+    this.pizzaEventSubscription=
+    this.sharedService.getClickEvent().subscribe((pizza) =>{
+      this.addToCart(pizza)
+    })
+  }
 
   toggleMenu(){
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  toggleCartDropdown() {
+    this.isCartDropdownOpen = !this.isCartDropdownOpen;
+  }
+
+  removeFromCart(item: any) {
+    const index = this.cart.indexOf(item);
+    if (index > -1) {
+      this.cart.splice(index, 1);
+    }
+  }
+
+  addToCart(pizza: PizzaModel){
+    this.cart.push(pizza);
   }
 }
